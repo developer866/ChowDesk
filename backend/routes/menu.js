4a959e3bd5c2df01c941f2d31ef01f5b13c346a2
Developer866 <opeyemijoseph866@gmail.com>
2026-06-16 00:33:18 +0100

    fix: correct import path for menu controller in menu routes

--- backend/routes/menu.js
+++ backend/routes/menu.js
@@ -6,7 +6,7 @@ import {
   createMenuItem,
   updateMenuItem,
   deleteMenuItem,
-} from "../controllers/menuController.js";
+} from "../controllers/menucontroller.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
